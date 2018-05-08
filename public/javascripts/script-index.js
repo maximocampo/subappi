e8ceb0c59266bc7a382594832cdc505b3747cbc5
GermanOtniel <otnielgs94@gmail.com>
2018-05-08 11:04:21 -0500

    hidden

---
+++ public/javascripts/script-index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  console.log('IronGenerator JS imported successfully!');
+    
+  $("#a-iniciaSesion").click(function(){
+    $("#a-registrate").toggleClass("hidden")
+    $("#a-iniciaSesion").toggleClass("hidden")
+  })  
+  
+  }, false);
